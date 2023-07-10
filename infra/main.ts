7569e6a8bb16e684b565f834da23607eca4471a2
Sebastian Käßinger <s.kaessinger@micromata.de>
2023-07-10 21:35:04 +0200

    added frontend bucket and its deployment

--- infra/main.ts
+++ infra/main.ts
@@ -2,7 +2,9 @@ import { App } from "cdktf"
 
 import { FunctionStack } from "./src/stacks/function-stack"
 import { prefixedId } from "./src/util/names"
+import {FrontendStack} from "./src/stacks/frontend-stack";
 
 const app = new App()
 new FunctionStack(app, prefixedId("function-stack"))
+new FrontendStack(app, prefixedId('frontend-stack'))
 app.synth()
--- infra/src/util/names.ts
+++ infra/src/util/names.ts
@@ -1,4 +1,4 @@
-const PROJECT_PREFIX = "ht-aws-workshop"
+const PROJECT_PREFIX = "-aws-workshop" // todo add your initials here!
 
 export const prefixedId = (id: string) => {
   return `${PROJECT_PREFIX}-${id}`
