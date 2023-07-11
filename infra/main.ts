895a11dd1141502e1fefeba6a80fcddf47ca861e
Hagen Trinter <h.trinter@micromata.de>
2023-07-11 18:37:42 +0200

    add api stack with mock endpoint

--- infra/main.ts
+++ infra/main.ts
@@ -2,9 +2,11 @@ import { App } from "cdktf"
 
 import { FunctionStack } from "./src/stacks/function-stack"
 import { prefixedId } from "./src/util/names"
-import {FrontendStack} from "./src/stacks/frontend-stack";
+import { FrontendStack } from "./src/stacks/frontend-stack"
+import { ApiStack } from "./src/stacks/api-stack"
 
 const app = new App()
 new FunctionStack(app, prefixedId("function-stack"))
-new FrontendStack(app, prefixedId('frontend-stack'))
+new FrontendStack(app, prefixedId("frontend-stack"))
+new ApiStack(app, prefixedId("api-stack"))
 app.synth()
---
+++ infra/src/stacks/api-stack.ts
@@ -0,0 +1,57 @@
+import { TerraformStack } from "cdktf"
+import { Construct } from "constructs"
+import { prefixedId } from "../util/names"
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider"
+import { ApiGatewayRestApi } from "@cdktf/provider-aws/lib/api-gateway-rest-api"
+import { ApiGatewayDeployment } from "@cdktf/provider-aws/lib/api-gateway-deployment"
+import { ApiGatewayStage } from "@cdktf/provider-aws/lib/api-gateway-stage"
+import { ApiGatewayMethod } from "@cdktf/provider-aws/lib/api-gateway-method"
+import { ApiGatewayResource } from "@cdktf/provider-aws/lib/api-gateway-resource"
+import { ApiGatewayIntegration } from "@cdktf/provider-aws/lib/api-gateway-integration"
+
+export class ApiStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id)
+
+    new AwsProvider(this, prefixedId("aws-provider"), {
+      region: "eu-west-1"
+    })
+
+    const api = new ApiGatewayRestApi(this, prefixedId("api"), {
+      name: prefixedId("api")
+    })
+
+    const helloWorldResource = new ApiGatewayResource(this, prefixedId("hello-world-resource"), {
+      restApiId: api.id,
+      parentId: api.rootResourceId,
+      pathPart: "hello-world"
+    })
+
+    const helloWorldMethod = new ApiGatewayMethod(this, prefixedId("hello-world"), {
+      restApiId: api.id,
+      resourceId: helloWorldResource.id,
+      httpMethod: "GET",
+      authorization: "NONE"
+    })
+
+    new ApiGatewayIntegration(this, "HelloWorldMockIntegration", {
+      restApiId: api.id,
+      resourceId: helloWorldResource.id,
+      httpMethod: helloWorldMethod.httpMethod,
+      type: "MOCK"
+    })
+
+    const deployment = new ApiGatewayDeployment(this, prefixedId("api-deployment"), {
+      restApiId: api.id,
+      triggers: {
+        redeploymentTrigger: Date.now().toString() // triggers stage redeployment on every stack deployment
+      }
+    })
+
+    new ApiGatewayStage(this, prefixedId("api-stage"), {
+      restApiId: api.id,
+      deploymentId: deployment.id,
+      stageName: "latest"
+    })
+  }
+}
