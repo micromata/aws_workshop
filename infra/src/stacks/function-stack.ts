e23c83e5a47cefbc6f141997fd454649e08beab1
Hagen Trinter <h.trinter@micromata.de>
2023-07-12 18:28:02 +0200

    stage3

--- infra/src/stacks/function-stack.ts
+++ infra/src/stacks/function-stack.ts
@@ -1,14 +1,54 @@
-import { TerraformStack } from "cdktf"
+import { AssetType, TerraformAsset, TerraformStack } from "cdktf"
 import { Construct } from "constructs"
-import { prefixedId } from "../util/names"
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider"
+import { IamRole } from "@cdktf/provider-aws/lib/iam-role"
+import { LambdaFunction } from "@cdktf/provider-aws/lib/lambda-function"
+
+import { prefixedId } from "../util/names"
 
 export class FunctionStack extends TerraformStack {
+  readonly chatLambdaFunction: LambdaFunction
+
   constructor(scope: Construct, id: string) {
     super(scope, id)
 
     new AwsProvider(this, prefixedId("aws-provider"), {
       region: "eu-west-1"
+    })
+
+    const lambdaAsset = new TerraformAsset(this, prefixedId("test-lambda-code"), {
+      path: "../lambda/dist/",
+      type: AssetType.ARCHIVE
+    })
+
+    const executionRole = new IamRole(this, prefixedId("test-lambda-role"), {
+      name: prefixedId("test-lambda-role"),
+      assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Principal: {
+              Service: "lambda.amazonaws.com"
+            },
+            Effect: "Allow",
+            Sid: ""
+          }
+        ]
+      }),
+      managedPolicyArns: ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+    })
+
+    this.chatLambdaFunction = new LambdaFunction(this, prefixedId("test-function"), {
+      functionName: prefixedId("test-function"),
+      handler: "conversation.handler",
+      runtime: "nodejs18.x",
+      filename: lambdaAsset.path,
+      role: executionRole.arn,
+      timeout: 15,
+      environment: {
+        variables: {}
+      }
     })
   }
 }
