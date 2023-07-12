92aae9547c796acc30566acb58c75f1c6a13b32c
Hagen Trinter <h.trinter@micromata.de>
2023-07-12 18:54:58 +0200

    stage6

--- infra/main.ts
+++ infra/main.ts
@@ -3,9 +3,11 @@ import { App } from "cdktf"
 import { prefixedId } from "./src/util/names"
 import { FrontendStack } from "./src/stacks/frontend-stack"
 import { FunctionStack } from "./src/stacks/function-stack"
+import { ApiStack } from "./src/stacks/api-stack"
 
 const app = new App()
 new FrontendStack(app, prefixedId("frontend-stack"))
 new FunctionStack(app, prefixedId("function-stack"))
+new ApiStack(app, prefixedId("api-stack"))
 
 app.synth()
