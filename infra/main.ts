3312e120f77f3cdc8d6725d753ff01e292ba8836
Hagen Trinter <h.trinter@micromata.de>
2023-07-12 18:11:15 +0200

    stage1

--- infra/main.ts
+++ infra/main.ts
@@ -1,19 +1,9 @@
 import { App } from "cdktf"
 
-import { FunctionStack } from "./src/stacks/function-stack"
 import { prefixedId } from "./src/util/names"
 import { FrontendStack } from "./src/stacks/frontend-stack"
-import { ApiStack } from "./src/stacks/api-stack"
 
 const app = new App()
-const functionStack = new FunctionStack(app, prefixedId("function-stack"))
-const frontendStack = new FrontendStack(app, prefixedId("frontend-stack"))
-const apiStack = new ApiStack(app, prefixedId("api-stack"), {
-  chatLambdaFunction: functionStack.chatLambdaFunction,
-  frontendBucket: frontendStack.frontendBucket
-})
-
-apiStack.addDependency(functionStack)
-apiStack.addDependency(frontendStack)
+new FrontendStack(app, prefixedId("frontend-stack"))
 
 app.synth()
