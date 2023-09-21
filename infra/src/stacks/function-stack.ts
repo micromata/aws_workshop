0816d8d61055c935377710d66e42d73084438abc
Hagen Trinter <h.trinter@micromata.de>
2023-09-21 16:05:58 +0200

    add packaged dependency layer

--- infra/src/stacks/function-stack.ts
+++ infra/src/stacks/function-stack.ts
@@ -3,9 +3,9 @@ import { Construct } from "constructs"
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider"
 import { IamRole } from "@cdktf/provider-aws/lib/iam-role"
 import { LambdaFunction } from "@cdktf/provider-aws/lib/lambda-function"
+import { LambdaLayerVersion } from "@cdktf/provider-aws/lib/lambda-layer-version"
 
 import { prefixedId } from "../util/names"
-import { LambdaLayerVersion } from "@cdktf/provider-aws/lib/lambda-layer-version"
 
 export class FunctionStack extends TerraformStack {
   readonly chatLambdaFunction: LambdaFunction
@@ -18,8 +18,8 @@ export class FunctionStack extends TerraformStack {
     })
 
     const dependencyLayerAsset = new TerraformAsset(this, prefixedId("test-lambda-layer-asset"), {
-      path: "../lambda/dependency-layer/",
-      type: AssetType.ARCHIVE
+      path: "../lambda/dependency-layer.zip",
+      type: AssetType.FILE
     })
     const dependencyLayer = new LambdaLayerVersion(this, prefixedId("test-lambda-layer"), {
       layerName: prefixedId("test-lambda-layer"),
